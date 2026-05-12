3be7f637aa3bfc0f4054b9da7a308667b6fe7da7
TYeshua <thiagoyeshua01@gmail.com>
2026-05-12 00:34:41 -0300

    chore: setup github pages deployment

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import cesium from 'vite-plugin-cesium'; // 1. Importe o plugin
 
 export default defineConfig({
+  base: '/PetroGame/',
   plugins: [
     react(),
     cesium() // 2. Adicione à lista de plugins
